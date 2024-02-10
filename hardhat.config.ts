2de18f59b92024e65b0e23cb771eee8b2c0c42eb
aboudjem <boudjemaa.adam@gmail.com>
2024-02-10 18:51:23 +0400

    🔧 fix config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-foundry";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.23",
+    version: "0.8.24",
     settings: {
-      evmVersion: "paris",
-      optimizer: { enabled: true, runs: 1000000 },
+      optimizer: {
+        enabled: true,
+        runs: 1000000,
+        details: {
+          yul: true,
+        },
+      },
     },
   },
 };
